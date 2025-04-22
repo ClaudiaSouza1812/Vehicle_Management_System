1e6cb19a57e767b8725250f953face06649862a2
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2025-04-22 18:35:05 +0100

    20250422

--- E01_OOP_Vehicle_v1/Properties/AssemblyInfo.cs
+++ E01_OOP_Vehicle_v1/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 /* 
+
+*/
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -34,5 +36,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-*/
-
--- E01_OOP_Vehicle_v1/obj/Debug/net6.0/E01_OOP_Vehicle_v1.AssemblyInfo.cs
+++ E01_OOP_Vehicle_v1/obj/Debug/net6.0/E01_OOP_Vehicle_v1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E01_OOP_Vehicle_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9287ad22cf64a62b89b3ba5fb073e7bbdeec86d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc88dbc6d8c9e235caef359f47e3c04a1880261")]
 [assembly: System.Reflection.AssemblyProductAttribute("E01_OOP_Vehicle_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E01_OOP_Vehicle_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
